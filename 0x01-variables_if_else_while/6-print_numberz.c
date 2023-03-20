7f649f07fbbb7256752bfb19748d1b78a574a289
Odunlade <jonathanodunlade@gmail.com>
2023-03-20 15:52:57 -0700

    commit 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this prints numbers from o and 9
+ *
+ * Return : always 0 (when code is successful)
+ */
+
+
+int main(void)
+
+{
+
+    int c;
+
+    c = 0;
+    while 
+	   (c < 10 ) {
+		   putchar(c + '0');
+		   c++;
+	   }
+    putchar('\n');
+
+    return (0);
+}
