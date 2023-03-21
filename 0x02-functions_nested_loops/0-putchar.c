1296871eae30801f619e787d0325f8ed53cae66e
Odunlade <jonathanodunlade@gmail.com>
2023-03-21 11:51:11 -0700

    First commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - this prints putchar to standard output
+ *
+ *Return: 0 (when code is successful)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
