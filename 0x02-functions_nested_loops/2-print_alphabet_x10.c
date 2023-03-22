e079e5e059a5cb33307daf1047b69f36f3d0e9bf
Odunlade <jonathanodunlade@gmail.com>
2023-03-22 05:40:57 -0700

    commit 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * print_alphabet_x10 - prints the alphabet x10 times in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
+		putchar('\n');
+		i++;
+	}
+}
