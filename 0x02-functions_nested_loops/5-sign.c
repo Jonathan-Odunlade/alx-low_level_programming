e30e8cb299122ed244efc4a44b8dbac370a0e470
Odunlade <jonathanodunlade@gmail.com>
2023-03-22 08:07:46 -0700

    commit 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the character to be checked
+ * Return: 0/1/-1 if n is zero/more than zero/less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar(45);
+		return (0);
+	}
+	else
+	{
+		_putchar(48);
+		return (-1);
+	}
+}
