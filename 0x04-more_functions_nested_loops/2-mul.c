bde215cf0c1f8f454f61296fa4c765df36f7bfc0
Odunlade <jonathanodunlade@gmail.com>
2023-03-23 16:45:36 -0700

    commit 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: one of the chars to be multiplied
+ * @b: the second char
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
