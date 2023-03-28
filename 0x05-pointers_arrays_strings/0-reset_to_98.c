e8304d6225440eb0906ba9d4e17131f20a402276
Odunlade <jonathanodunlade@gmail.com>
2023-03-27 19:17:05 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the valur of n to 98 by deferencing
+ * @n: the int to be changed
+ * return: 0 if code successful
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
